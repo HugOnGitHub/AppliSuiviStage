35f7758c75b07d015c77a5b722ce5b1e7fd63474
fmoinard <fmoinard@la-joliverie.com>
2021-03-16 16:47:29 +0100

    test

--- app/src/main/java/com/example/applisuivistage/AfficheStages.java
+++ app/src/main/java/com/example/applisuivistage/AfficheStages.java
@@ -14,5 +14,7 @@ public class AfficheStages extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toutafficher);
+
+
     }
 }
